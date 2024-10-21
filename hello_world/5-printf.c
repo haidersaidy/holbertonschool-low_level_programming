74f78c6600ee02cd58525f6a77d3e6fe5578aa19
haidersaidy <haidersaidy666@gmail.com>
2024-10-21 12:08:43 +0000

    print

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,17 +1,19 @@
-/*                                                                              
- * File: 4-puts.c                                                               
- * Auth: haidersaidy <9973@holbertonstudents.com>                               
+/*
+ * File: 5-printf.c
+ * Auth: haidersaidy
  */
 
-#include <stdio.h>    
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{ 
-/**                                                                           
- * main - Prints "with proper grammar, but the outcome is a piece of art,.                             
- *                                                                            
- * Return: Always 0.                                                          
- */   
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-	       
-        return 0;
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
