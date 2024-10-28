7c202141d6c861a85f5823351c88e1f4fc9f3ddd
haidersaidy <haidersaidy666@gmail.com>
2024-10-28 11:36:53 +0100

    fixed

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-// main.h
 #ifndef MAIN_H
 #define MAIN_H
 
+int _isupper(int c);
+int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
 
-#endif /* MAIN_H */
-
+#endif
