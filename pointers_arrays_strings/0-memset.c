db0a3a24a3115191d16b881bc2a43dec88f634f2
haidersaidy <haidersaidy666@gmail.com>
2024-11-06 11:50:51 +0100

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to the memory area
@@ -7,10 +6,12 @@
  *
  * Return: pointer to the memory area s
  */
-char *_memset(char *s, char b, unsigned int n) 
+char *_memset(char *s, char b, unsigned int n)
 {
-for (unsigned int i = 0; i < n; i++) {
+unsigned int i;
+for (i = 0; i < n; i++)
+{
 s[i] = b;
 }
-return s;
+return (s);
 }
